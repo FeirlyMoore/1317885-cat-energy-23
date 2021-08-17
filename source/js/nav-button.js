592113b92d44d5ad019847b1e4d8c2f1249d3252
REDFOX39 <daniil.fox1998@yandex.ru>
2021-08-17 03:20:28 +0300

    Стилизация всех страниц

---
+++ source/js/nav-button.js
@@ -0,0 +1,20 @@
+let navToggle = document.querySelector(".nav-toggle-button--js");
+let nav = document.querySelector(".main-nav");
+let indexNav = document.querySelector(".nav-list__item-index--js");
+let partnership = document.querySelector(".partnership");
+
+navToggle.onclick = function () {
+  if (nav.classList.contains("main-nav--closed")) {
+    nav.classList.remove("main-nav--closed");
+    nav.classList.add("main-nav--opened");
+  } else {
+    nav.classList.remove("main-nav--opened");
+    nav.classList.add("main-nav--closed");
+  }
+}
+
+if (indexNav.classList.contains("nav-list__item--current")) {
+  partnership.style.background = "#ffffff";
+}
+
+//<!DOCTYPE Liky>
