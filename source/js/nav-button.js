d52a8abe4b66ff875bb1a841ab9835345bab4272
REDFOX39 <daniil.fox1998@yandex.ru>
2021-08-20 00:27:54 +0300

    Декомпозиция стилей по блокам

--- source/js/nav-button.js
+++ source/js/nav-button.js
@@ -1,7 +1,5 @@
 let navToggle = document.querySelector(".nav-toggle-button--js");
 let nav = document.querySelector(".main-nav");
-let indexNav = document.querySelector(".nav-list__item-index");
-let partnership = document.querySelector(".partnership");
 
 navToggle.onclick = function () {
   if (nav.classList.contains("main-nav--closed")) {
@@ -11,10 +9,6 @@ navToggle.onclick = function () {
     nav.classList.remove("main-nav--opened");
     nav.classList.add("main-nav--closed");
   }
-}
-
-if (indexNav.classList.contains("nav-list__item--current")) {
-  partnership.style.background = "#ffffff";
 }
 
 //<!DOCTYPE Liky>
---
+++ source/js/scroll.js
@@ -0,0 +1,14 @@
+let scrollLine = document.getElementById("page_progress");
+
+window.onscroll = function() {
+    let pageHeight = document.documentElement.scrollHeight;
+    let innerHeight = window.innerHeight;
+    let scroll = window.pageYOffset;
+
+    let progressPercent = scroll/((pageHeight-innerHeight)/100);
+    if (progressPercent == NaN) {
+        scrollLine.value = 0;
+    } else {
+        scrollLine.value = progressPercent;
+    }
+}
