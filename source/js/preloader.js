fee74e63deebb346af70b7ddc5cda5744ea47870
REDFOX39 <daniil.fox1998@yandex.ru>
2021-09-06 00:39:59 +0300

    Доработка адаптивности блоков

--- source/js/preloader.js
+++ source/js/preloader.js
@@ -1,10 +1,10 @@
-document.body.classList.remove('loaded--hiding');
+document.body.classList.remove('loaded-hiding');
 
 window.onload = function () {
-    document.body.classList.add('loaded--hiding');
+    document.body.classList.add('loaded-hiding');
     window.setTimeout(function () {
       document.body.classList.add('loaded');
-      document.body.classList.remove('loaded--hiding');
+      document.body.classList.remove('loaded-hiding');
     }, 500);
   }
 
