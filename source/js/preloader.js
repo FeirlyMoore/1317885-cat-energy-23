b398a1aa0326d025555088cefc64794625a0dbe0
REDFOX39 <daniil.fox1998@yandex.ru>
2021-09-07 23:44:43 +0300

    Анимация блоков

--- source/js/preloader.js
+++ source/js/preloader.js
@@ -1,11 +1,38 @@
 document.body.classList.remove('loaded-hiding');
 
-window.onload = function () {
-    document.body.classList.add('loaded-hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded-hiding');
-    }, 500);
-  }
+window.onload = () => {
+  document.body.classList.add('loaded-hiding');
+
+  window.setTimeout(() => {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded-hiding');
+
+    const products = document.querySelectorAll('.products__item');
+    for (product of products) {
+      product.classList.add('animation-scale');
+      // product.style.animation = "animation-scale 1s ease";
+    }
+
+    const promoWrapper = document.querySelector('.promo__title-wrapper');
+    if (promoWrapper) promoWrapper.style.animation = "animation-scale 1s ease";
+
+    const promoButton = document.querySelector('.promo-button');
+    if (promoButton) promoButton.style.animation = "animation-scale 1s ease";
+
+    const promoPicture = document.querySelector('.promo__picture');
+    if (promoPicture) promoPicture.style.animation = "animation-position 1s ease";
+
+    const privilegesItem = document.querySelectorAll('.privileges__list-item');
+    if (privilegesItem) {
+      for (let i = 0; i < privilegesItem.length; i++) {
+        let j = 1.5 + i;
+        privilegesItem[i].style.animation = `animation-scale ${j}s ease`;
+        // privilegesItem[i].style = `--animation-duration: ${j}s`;
+        // j++;
+      }
+    }
+
+  }, 500);
+}
 
 //<!DOCTYPE Liky>
