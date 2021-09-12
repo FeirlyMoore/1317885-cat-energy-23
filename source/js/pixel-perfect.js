9c31d4ac9be09e7ea0462405a5275e77ea6d5f75
REDFOX39 <daniil.fox1998@yandex.ru>
2021-09-12 03:19:56 +0300

    Pixel perfect главной страницы ( Завершено на 78.5% ), незначительный апгрэйд PP

--- source/js/pixel-perfect.js
+++ source/js/pixel-perfect.js
@@ -1,13 +1,21 @@
 const ppButton = document.querySelector('.pixel-perfect__button');
 const ppImg = document.querySelector('.pixel-perfect__img');
+const pixelPerfect = document.querySelector('.pixel-perfect');
 
 ppImg.style.opacity = 0;
+pixelPerfect.classList.add('hidden');
 
 let ppOpValue = ppImg.style.opacity;
 
 let i = 0.1;
 
 document.onkeydown = function(e) {
+  if (ppImg.style.opacity <= 0) {
+    pixelPerfect.classList.add('hidden');
+  } else if (ppImg.style.opacity > 0) {
+    pixelPerfect.classList.remove('hidden');
+  }
+
   switch (e.keyCode) {
     case 87:
       ppOpValue = Number(ppOpValue) + i;
